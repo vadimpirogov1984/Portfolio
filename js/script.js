096e8289b63854f05f6660a58b7e8c01781dfa64
Vadim <vadim_pirogov@mail.ru>
2022-12-14 21:15:27 +0300

    Added active class for page's link

--- js/script.js
+++ js/script.js
@@ -262,3 +262,19 @@ $(document).ready(function() {
 	});
 });
   
+
+//Добваление класса активной ссылки страницы=========================
+activeLink();
+
+function activeLink()
+{
+    const links = document.getElementsByTagName("a");
+
+    for(var i=0;i<links.length;i++)
+    {
+        if(links[i].href === window.location.href)
+        {
+            links[i].classList.add('active-link');  
+        }
+    }  
+};
